6995e807f5182acb6d5a3f9dae7f241e31644218
Falcon Mods <47052459+Falcon1205@users.noreply.github.com>
2020-02-16 19:00:26 +1100

    Add files via upload

--- LuminiteNPC.cs
+++ LuminiteNPC.cs
@@ -1,9 +1,26 @@
+using Terraria.Graphics.Effects;
+using Terraria.Graphics.Shaders;
+using Terraria.ID;
+using Terraria.Localization;
 using Terraria.ModLoader;
+using Terraria.UI;
 
 namespace LuminiteNPC
 {
 	public class LuminiteNPC : Mod
 	{
+		public static LuminiteNPC Instance;
+
+		public override void Load()
+		{
+			Instance = this;
+		}
+
+		public override void Unload()
+		{
+			Instance = null;
+		}
+
 		public LuminiteNPC()
 		{
 		}
